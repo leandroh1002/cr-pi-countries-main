7daa8bbc53aa343992029a218551911ed7819aef
leandroh1002 <leandroherrera1002@gmail.com>
2023-12-04 17:02:13 -0300

    change in detail, check data again

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -5,34 +5,34 @@ import { useState, useEffect } from 'react'
 
 
 function Detail() {
-    const [character, setCharacter] = useState([]);
+    const [countries, setCountries] = useState([]);
     const {id} = useParams();
 
-    // useEffect(() => {
-    //     axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
-    //        if (data.name) {
-    //           setCharacter(data);
-    //        } else {
-    //           window.alert('No hay personajes con ese ID');
-    //        }
-    //     });
-    //     return setCharacter({});
-    //  }, [id]);
+    useEffect(() => {
+        axios(`http://localhost:3001/api/countries/${id}`).then(({ data }) => {
+           if (data.name) {
+              setCountries(data);
+           } else {
+              window.alert('No hay countries con ese ID');
+           }
+        });
+        return setCountries({});
+     }, [id]);
 
   return (
    <div className={styles.container}>
       <div className={styles.divcardsdetail}>
          <div >
-            <img className={styles.divheader} src={character.image} />
+            <img className={styles.divheader} src={countries.image} />
          </div>
 
          <div className={styles.div}>
-            <h1 className={styles.divh1}>{character.name}</h1>
+            <h1 className={styles.divh1}>{countries.name}</h1>
                <div className={styles.divh2}>
-               <h2>Status: {character.status}</h2>
-               <h2>Especie: {character.species}</h2>
-               <h2>Genero: {character.gender}</h2>
-               <h2>{character.origin && character.origin.name && (<>Origen: {character.origin.name}</>)}</h2>
+               {/* <h2>Status: {countries.status}</h2> //! Va a depender de como traiga axios la Data, hay que hacer console.log
+               <h2>Especie: {countries.species}</h2>
+               <h2>Genero: {countries.gender}</h2>
+               <h2>{countries.origin && countries.origin.name && (<>Origen: {countries.origin.name}</>)}</h2> */}
                </div>
          </div>
       </div>
