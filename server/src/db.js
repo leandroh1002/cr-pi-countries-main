ba033f48d9a838c37aa745e98bec200e6feff64c
leandroh1002 <leandroherrera1002@gmail.com>
2024-01-18 22:48:45 -0300

    fix in env now with env tested

--- server/src/db.js
+++ server/src/db.js
@@ -4,10 +4,10 @@ const { Sequelize } = require("sequelize");
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_DEPLOY
+  DB_USER, DB_PASSWORD, DB_HOST,DB_DEPLOY, DB_DATABASE
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_DEPLOY_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`, {
   logging: false, 
   native: false, 
   dialectOptions: {
